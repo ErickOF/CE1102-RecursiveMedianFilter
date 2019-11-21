16b1ec72d595dbca8f0f31b6dcd073ad9c737d2c
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-11-21 10:09:50 -0600

    Filter algorithm was added

--- imgfilter.py
+++ imgfilter.py
@@ -1,6 +1,20 @@
-import numpy
+import numpy as np
 
 
 class ImageFilter:
-    def medians(img, windows_size):
-        return img
+    def medians(self, img, windows_size):
+        filtered = np.zeros(img.shape)
+        return self.__medians_aux(img, filtered, windows_size, 0, 0)
+
+    def __medians_aux(self, img, filtered, windows_size, i, j):
+        print(i, j)
+        if i == img.shape[0]:
+            return filtered
+        elif j == img.shape[1]:
+            return self.__medians_aux(img, filtered, windows_size, i + 1, 0)
+        else:
+            filtered[i][j] = self.__get_new_pixel(img, windows_size, i, j)
+            return self.__medians_aux(img, filtered, windows_size, i, j + 1)
+
+    def __get_new_pixel(self, img, windows_size, i, j):
+        return img[i][j]
--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-from scipy import misc;
 import matplotlib.pyplot as plt
-import sys;
+import sys
 
 from imgfilter import ImageFilter
 
 
 if __name__ == '__main__':
     # Change the recursion limit
-    sys.setrecursionlimit(10**6);
-    print('Test 1 running');
+    sys.setrecursionlimit(10**9)
+    print('Test 1 running')
     # Read image in gray scale
-    gray_img = misc.imread('imgs/input/manNoisy.png', 1);
+    # imgs/input/manNoisy.png
+    img_path = input('Input image name: ')
+    # imgs/output/filter.jpg
+    out_path = input('Output image name: ')
+    gray_img = plt.imread(img_path, 1)
     # Show image in gray scale
-    plt.imshow(gray_img, plt.get_cmap('gray'));
-    plt.show();
+    plt.imshow(gray_img, plt.get_cmap('gray'))
+    plt.show()
     # Filter image
-    window_size = 3;
+    window_size = int(input('Windows size: '))
     img_filter = ImageFilter()
-    filtered_img = img_filter.medians(gray_img, window_size);
-    
-    misc.imsave('imgs/output/filter.jpg', filtered_img);
-    misc.imsave('imgs/output/original.jpg', gray_img);
-    print('Finish!');
-    plt.imshow(filtered_img, plt.get_cmap('gray'));
-    plt.show();
+    filtered_img = img_filter.medians(gray_img, window_size)
+
+    plt.imsave(out_path, filtered_img)
+    print('Finish!')
+    plt.imshow(filtered_img, plt.get_cmap('gray'))
+    plt.show()
