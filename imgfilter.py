877a04ba86226dfc506027bdef9568f42af61d6a
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2024-09-18 01:40:30 -0600

    Updating source code with better comments and typing hints

--- imgfilter.py
+++ imgfilter.py
@@ -1,107 +1,174 @@
+"""
+Filter Class for Image Processing
+
+This class provides a median filter for grayscale images. The filter is applied using a recursive 
+approach to traverse and process the image. The median filter helps in reducing noise while 
+preserving edges by taking the median of the pixels in a specified window around each pixel.
+
+The class provides the following functionalities:
+- Median filtering of an image (`median` method).
+- Recursive filtering through rows and columns (`__filter` and `__filter_row` methods).
+- Extracting the median of a given window in the image (`__get_median` method).
+- Sorting the window using the bubble sort algorithm (`__sort` method).
+
+Usage:
+1. Instantiate the `Filter` class with the number of rows and columns of the image.
+2. Call the `median` method to apply the median filter to the image.
+
+Author: Erick Andres Obregon Fonseca
+"""
 import numpy as np
+
+from typing import Final
 
 
 class Filter:
+    """
+    Filter Class for Image Processing
+
+    This class provides a median filter for grayscale images. The filter is
+    applied using a recursive approach to traverse and process the image. The
+    median filter helps in reducing noise while  preserving edges by taking
+    the median of the pixels in a specified window around each pixel.
+
+    Usage:
+    1. Instantiate the `Filter` class with the number of rows and columns of
+    the image.
+    2. Call the `median` method to apply the median filter to the image.
+    """
+
     def __init__(self, row_number: int, col_number: int):
-        """Filter class
+        """
+        Initializes the Filter class with the specified number of rows and
+        columns.
 
         Args:
-            row_number (int): number of rows
-            col_number (int): number of columns
+            row_number (int): Number of rows in the image.
+            col_number (int): Number of columns in the image.
         """
-        self.__row_number: int = row_number
-        self.__col_number: int = col_number
+        self.__row_number: Final[int] = row_number
+        self.__col_number: Final[int] = col_number
 
-    def median(self, gray_img: np.ndarray, window_size: int,
-               filtered_img: np.ndarray) -> None:
-        """Filter image using median method
+    def median(
+            self,
+            gray_img: np.ndarray,
+            window_size: int,
+            filtered_img: np.ndarray
+        ) -> None:
+        """
+        Apply a median filter to the image using the specified window size.
 
         Args:
-            gray_img (np.ndarray): image to filter
-            window_size (int): window size of the filter
-            filtered_img (np.ndarray): output with the filtered image
+            gray_img (np.ndarray): Grayscale image to filter.
+            window_size (int): Size of the window to calculate the median.
+            filtered_img (np.ndarray): Output array to store the filtered
+                                       image.
         """
+        # Call the private filter method to apply the median filter
         self.__filter(gray_img, window_size // 2, filtered_img)
 
-    def __filter(self, gray_img: np.ndarray, mid_window: int,
-                 filtered_img: np.ndarray, i: int = 0) -> None:
-        """Auxiliary function
+    def __filter(
+            self,
+            gray_img: np.ndarray,
+            mid_window: int,
+            filtered_img: np.ndarray,
+            i: int = 0
+        ) -> None:
+        """
+        Recursively filter the image by processing each row.
 
         Args:
-            gray_img (np.ndarray): image to filter
-            mid_window (int): mid of the window
-            filtered_img (np.ndarray): output with the filtered image
-            i (int): current row
+            gray_img (np.ndarray): Grayscale image to filter.
+            mid_window (int): Half the window size.
+            filtered_img (np.ndarray): Output array to store the filtered
+                                       image.
+            i (int): Current row being processed.
         """
-        # End of the image
-        if (i == self.__row_number - mid_window):
+        # End of the image; stop recursion
+        if i == (self.__row_number - mid_window):
             return
-        # Filter image
         else:
-            # Filter row
+            # Filter the current row
             self.__filter_row(gray_img, mid_window, filtered_img, i)
-            # Next row
+            # Recur to the next row
             self.__filter(gray_img, mid_window, filtered_img, i + 1)
     
-    def __filter_row(self, gray_img: np.ndarray, mid_window: int,
-                     filtered_img: np.ndarray, i: int, j: int = 0) -> None:
-        """Filter by row
+    def __filter_row(
+            self, 
+            gray_img: np.ndarray,
+            mid_window: int,
+            filtered_img: np.ndarray,
+            i: int,
+            j: int = 0
+        ) -> None:
+        """
+        Recursively filter each pixel in the current row.
 
         Args:
-            gray_img (np.ndarray): image to filter
-            mid_window (int): mid of the window
-            filtered_img (np.ndarray): output with the filtered image
-            i (int): current row
-            j (int): current column
+            gray_img (np.ndarray): Grayscale image to filter.
+            mid_window (int): Half the window size.
+            filtered_img (np.ndarray): Output array to store the filtered
+                                       image.
+            i (int): Current row being processed.
+            j (int): Current column being processed.
         """
-        # End of the row
-        if (j == self.__col_number - mid_window):
+        # End of the row; stop recursion
+        if j == (self.__col_number - mid_window):
             return
         else:
-            # Get the new pixel for the image
+            # Get the median value for the current window and update the pixel
             filtered_img[i, j] = self.__get_median(gray_img, mid_window, i, j)
-            # Next pixel
+            # Recur to the next pixel in the row
             self.__filter_row(gray_img, mid_window, filtered_img, i, j + 1)
 
-    def __get_median(self, gray_img: np.ndarray, mid_window: int, i: int,
-                     j: int) -> float:
-        """Get the median of the window
+    def __get_median(
+            self,
+            gray_img: np.ndarray,
+            mid_window: int,
+            i: int,
+            j: int
+        ) -> float:
+        """
+        Calculate the median of the pixel values in the specified window.
 
         Args:
-            gray_img (np.ndarray): input image
-            mid_window (int): mid of the window
-            i (int): current row of the image
-            j (int): current column of the image
+            gray_img (np.ndarray): Grayscale image.
+            mid_window (int): Half the window size.
+            i (int): Current row of the image.
+            j (int): Current column of the image.
 
         Returns:
-            float: median of the window
+            float: Median of the pixel values in the window.
         """
-        # Get window
+        # Extract the window around the current pixel and flatten it into a 1D
+        # array
         window: np.ndarray = gray_img[max(0, i - mid_window):min(i + mid_window + 1, self.__row_number),
                                       max(0, j - mid_window):min(j + mid_window + 1, self.__col_number)].flatten()
+        # Sort the window to find the median
         self.__sort(window, window.size)
 
-        # Return median of the window
+        # Return the median value of the window
         return window[window.size // 2]
 
     def __sort(self, array: np.array, i: int, j: int = 1) -> None:
-        """Sorting using bubble algorithm
+        """
+        Sort the array using the bubble sort algorithm.
 
         Args:
-            array (np.array): array to sort
-            i (int): current index of the array
-            j (int): pivote
+            array (np.array): Array to be sorted.
+            i (int): Current index of the array being processed.
+            j (int): Pivot index for the current sorting pass.
         """
-        # Array is sorted
-        if (i == 0):
+        # Array is sorted; stop recursion
+        if i == 0:
             return
-        # Start again
-        elif (j == i):
+        # Reset j to 1 and reduce i for the next pass
+        elif j == i:
             self.__sort(array, i - 1, 1)
         else:
-            # Check if the previous element is greater
-            if (array[j - 1] > array[j]):
+            # Swap if the previous element is greater than the current element
+            if array[j - 1] > array[j]:
                 array[j - 1], array[j] = array[j], array[j - 1]
 
-            # Next element
+            # Recursive to the next element in the current pass
             self.__sort(array, i, j + 1)
--- main.py
+++ main.py
@@ -1,60 +1,60 @@
-# -*- coding: utf-8 -*-
-'''
-Created on Tue Apr 12 15:36 2016
-Last update on Sat 16 10:57 2021
-'''
+"""
+This script applies a median filter to an image provided by the user.
+It converts the image to grayscale, applies the median filter with a specified
+window size, and then displays and saves the filtered image.
 
-__author__ = "Erick Andres Obregon Fonseca"
-__copyright__ = "Copyright 2021"
-__credits__ = []
-__license__ = "GPL"
-__version__ = "1.1"
-__maintainer__ = "Erick Andres Obregon Fonseca"
-__email__ = "erickobregonf@gmail.com"
-__status__ = "Production"
+Usage:
+1. Run the script.
+2. Provide the path to the image file to be filtered.
+3. Specify the window size for the median filter.
+4. Specify the path and name to save the filtered image.
 
-
+Author: Erick Andres Obregon Fonseca
+"""
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
 import sys
+
+from PIL import Image
+from typing import Tuple
 
 from imgfilter import Filter
 
 
 def main():
+    # Increase recursion limit to prevent RecursionError for large images
     sys.setrecursionlimit(10**9)
 
-    # Open image
+    # Open the image
     img_path: str = input('Path of the image: ')
-    gray_img = np.asarray(Image.open(img_path).convert('L'))
+    gray_img: np.ndarray = np.asarray(Image.open(img_path).convert('L'))
 
-    # Show gray image
+    # Display the grayscale image
     plt.imshow(gray_img, plt.get_cmap('gray'))
     plt.title('Gray image')
     plt.show()
 
-    # Get image dimensions
-    dim: tuple = gray_img.shape
+    # Get the dimensions of the image
+    dim: Tuple[int, int] = gray_img.shape
 
-    # Ask for window size
+    # Prompt the user to enter the filter's window size
     window_size: int = int(input('Window size of the filter: '))
 
-    # Empty image for result
+    # Create an empty array for the filtered image
     filtered_img: np.ndarray = np.zeros(dim)
 
-    # Filter instance
-    filter: Filter = Filter(dim[0], dim[1]) 
+    # Create a Filter instance for the image dimensions
+    filter: Filter = Filter(dim[0], dim[1])
 
-    # Filter image
+    # Apply the median filter to the image
     filter.median(gray_img, window_size, filtered_img)
 
-    # Show filtered image
+    # Display the filtered image
     plt.imshow(filtered_img, plt.get_cmap('gray'))
     plt.title('Filtered image')
     plt.show()
 
-    # Save filtered image
+    # Prompt the user to enter the path and name to save the filtered image
     filtered_img_path: str = input('Filtered image path and name to save: ')
     plt.imsave(filtered_img_path, filtered_img)
 
@@ -63,4 +63,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main() 
+    main()
